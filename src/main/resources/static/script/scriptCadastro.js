3fc0449b90a0192535d9fb70269aabbf155e6905
jucastrox <juliacstro1@gmail.com>
2025-05-15 09:40:30 -0300

    tentando atualizar o git

--- src/main/resources/static/script/scriptCadastro.js
+++ src/main/resources/static/script/scriptCadastro.js
@@ -52,5 +52,5 @@ document.addEventListener("DOMContentLoaded", function() {
 			console.error('Erro no cadastro:', error);
 			alert('Falha ao cadastrar pessoa. Tente novamente.');
 		});
-});
+});//teste 
 
