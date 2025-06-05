12852cd4d6941685d2632021551c49821e84577d
nathvng <nathaliagabrielevenegasiqueira@gmail.com>
2025-06-05 10:29:27 -0300

    data.sql com endereco e usuario

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -40,4 +40,18 @@ INSERT IGNORE INTO tb_produto (id_produto, nome_produto, descricao, preco, tipo_
 (19, 'Pulseira Infantil', 'Pulseira leve de bijuteria para crianças', 25.00, 3, 'pulseira4.jpg'),
 (20, 'Relógio Executivo', 'Relógio de prata com cronômetro', 650.00, 2, 'relogio4.jpg');
 
+INSERT IGNORE INTO tb_endereco (id_endereco, cep, rua, numero, bairro, cidade, uf, complemento) VALUES
+(1, 18015140,'Ramon Haro Martini',150, 'Vila Haro', 'Sorocaba', 'SP', 'TESTE'),
+(2, 18060110,'Escolástica Rosa de Almeida',180, 'Vila Carvalho', 'Sorocaba', 'SP', 'teste2'),
+(3, 18078000,'Gregório Ramos dos Santos',190, 'Jardim Santa Cecília', 'Sorocaba', 'SP', 'TESTE3'),
+(4, 18078110,'Victor Alfarano',200, 'Jardim Santa Camilo', 'Sorocaba', 'SP', 'TESTE4'),
+(5, 18020110,'Largo Francisco Eufrásio',210, 'Vila Hortencia', 'Sorocaba', 'SP', 'TESTE5');
+
+
+INSERT IGNORE INTO tb_usuario (id_usuario, nome_usuario, senha, cpf, email, telefone, data_nascimento, endereco_id_endereco, tipo_usuario) VALUES
+(1, 'Mariana Alves', 'mari123', '66985214578', 'mari@gmail.com', '15332458796', '2005-05-20', 1, 2),
+(2, 'Juliano Souza', 'juli123', '22657845210', 'juli@gmail.com', '15885214587', '2008-08-21', 2, 2),
+(3, 'Nathália Venega', 'nat123', '44587520012', 'nat@gmail.com', '15996589742', '2008-01-21', 3, 2),
+(4, 'Guilherme Jacob', 'gui123', '33697845210', 'gui@gmail.com', '15887542102', '2007-12-11', 4, 2),
+(5, 'Vedilson Silva', 'ved123', '33687521045', 'ved@gmail.com', '15662598754', '1985-07-10', 5, 2);
 
